6cdee60739d9e776f61c01216d9f87fd11276871
Daniel Tatsch <danielt.tatsch@gmail.com>
2020-10-04 16:24:54 -0300

    Adicionar classe de deteccao facial
    Por padrão o programa opera com a detecção de imagens de faces frontais. Arquivos de treinamento específicos podem ser passados como parâmetro no construtor da classe.

---
+++ projeto_final/face_detector.py
@@ -0,0 +1,17 @@
+import numpy as np
+from cv2 import cv2
+
+class face_detector:
+    def __init__(self, face_training_path:'haarcascade_frontalface_default.xml'):
+        self.face_cascade = cv2.CascadeClassifier(face_training_path)
+
+    # scaleFactor  - Especifica quanto a img eh reduzida para cada escala gerada (verificar funcionamento do algoritmo) 
+    # minNeighbors - Num de vizinhos que cada retangulo deve ter para a analise. Valores grandes resultam em menos deteccoes, mas com mais qualidade
+    # img          - Imagem em np.array
+    def detect(self, img, scaleFactor, minNeighbors):
+        gray  = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = self.face_cascade.detectMultiScale(gray, scaleFactor, minNeighbors)
+
+        return True if len(faces) != 0 else False
+        
+        # TODO: Gerar nova imagem de acordo com as coordernadas retornadas
