55e7908e435bcf1c1bf536b0e6d180ce3df63b03
Daniel Tatsch <danielt.tatsch@gmail.com>
2020-10-10 16:11:25 -0300

    Corrigir passagem de argumento default

--- projeto_final/face_detector.py
+++ projeto_final/face_detector.py
@@ -1,14 +1,14 @@
 import numpy as np
-from cv2 import cv2
+from   cv2 import cv2
 
 class face_detector:
-    def __init__(self, face_training_path:'haarcascade_frontalface_default.xml'):
+    def __init__(self, face_training_path='haarcascade_frontalface_default.xml'):
         self.face_cascade = cv2.CascadeClassifier(face_training_path)
 
     # scaleFactor  - Especifica quanto a img eh reduzida para cada escala gerada (verificar funcionamento do algoritmo) 
     # minNeighbors - Num de vizinhos que cada retangulo deve ter para a analise. Valores grandes resultam em menos deteccoes, mas com mais qualidade
     # img          - Imagem em np.array
-    def detect(self, img, scaleFactor, minNeighbors):
+    def detect(self, img, scaleFactor=2, minNeighbors=1):
         gray  = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         faces = self.face_cascade.detectMultiScale(gray, scaleFactor, minNeighbors)
 
