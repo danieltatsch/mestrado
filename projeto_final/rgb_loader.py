9235eb7387849e83fbd8f3e0b7dbced8bc1fc471
Daniel Tatsch <danielt.tatsch@gmail.com>
2020-10-10 18:03:13 -0300

    Generalizar nome do dicionario de imagens

--- projeto_final/rgb_loader.py
+++ projeto_final/rgb_loader.py
@@ -4,7 +4,7 @@ from   pprint import pprint
 
 class rgb_loader:
     def __init__(self, rap3df_database_path):
-        self.rgb_front_dict = {}
+        self.rgb_dict = {}
         
         database_02_structure_path = rap3df_database_path + 'rap3df_data_02/database.json'
         
@@ -12,22 +12,22 @@ class rgb_loader:
             data = json.load(json_file)
         
         for key, value in list(data.items())[:-1]:
-            self.rgb_front_dict[key] = {}
+            self.rgb_dict[key] = {}
 
-            self.rgb_front_dict[key]['front']  = rap3df_database_path + value['front'][0]['rgb']
-            self.rgb_front_dict[key]['left']   = rap3df_database_path + value['left'][0]['rgb']
-            self.rgb_front_dict[key]['right']  = rap3df_database_path + value['right'][0]['rgb']
-            self.rgb_front_dict[key]['up']     = rap3df_database_path + value['up'][0]['rgb']
-            self.rgb_front_dict[key]['down']   = rap3df_database_path + value['down'][0]['rgb']
-            self.rgb_front_dict[key]['burned'] = rap3df_database_path + value['burned'][0]['rgb']
+            self.rgb_dict[key]['front']  = rap3df_database_path + value['front'][0]['rgb']
+            self.rgb_dict[key]['left']   = rap3df_database_path + value['left'][0]['rgb']
+            self.rgb_dict[key]['right']  = rap3df_database_path + value['right'][0]['rgb']
+            self.rgb_dict[key]['up']     = rap3df_database_path + value['up'][0]['rgb']
+            self.rgb_dict[key]['down']   = rap3df_database_path + value['down'][0]['rgb']
+            self.rgb_dict[key]['burned'] = rap3df_database_path + value['burned'][0]['rgb']
 
-    def get_rgb_front_dict(self):
-        return self.rgb_front_dict
+    def get_rgb_dict(self):
+        return self.rgb_dict
 
     def get_rgb_pictures(self, position):
         rgb_front_pictures = {}
 
-        for key, value in list(self.rgb_front_dict.items()):
+        for key, value in list(self.rgb_dict.items()):
             rgb_front_pictures[key] = {}
 
             rgb_front_pictures[key][position]  = cv2.imread(value[position], cv2.COLOR_BGR2RGB)
