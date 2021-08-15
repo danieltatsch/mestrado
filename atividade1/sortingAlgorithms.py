e5243b58b2ab829ddc91f0789853f8bbf3b79eaf
Daniel Tatsch <danielt.tatsch@gmail.com>
2021-08-15 19:38:29 -0300

    Add Merge Sort Algorithm

--- atividade1/sortingAlgorithms.py
+++ atividade1/sortingAlgorithms.py
@@ -8,3 +8,35 @@ def insertion_sort(data):
             i -= 1
 
         data[i + 1] = key
+
+def merge_sort(data):
+    if len(data) > 1:
+        middle     = len(data)//2
+        left_list  = data[:middle]
+        right_list = data[middle:]
+
+        merge_sort(left_list)
+        merge_sort(right_list)
+
+        i = 0
+        j = 0
+        k = 0
+
+        while i < len(left_list) and j < len(right_list):
+            if left_list[i] < right_list[j]:
+                data[k] = left_list[i]
+                i += 1
+            else:
+                data[k] = right_list[j]
+                j += 1
+            k += 1
+
+        while i < len(left_list):
+            data[k] = left_list[i]
+            i += 1
+            k += 1
+
+        while j < len(right_list):
+            data[k] = right_list[j]
+            j += 1
+            k += 1
