9ffd63b700552dc8204e5224b300443112409101
Daniel Tatsch <danielt.tatsch@gmail.com>
2021-08-15 18:47:22 -0300

    Add Insertion Sort Algorithm

---
+++ atividade1/sortingAlgorithms.py
@@ -0,0 +1,10 @@
+def insertion_sort(data):
+    for j in range(1, len(data)):
+        key = data[j]
+        i = j - 1
+
+        while i >= 0 and data[i] > key:
+            data[i + 1] = data[i]
+            i -= 1
+
+        data[i + 1] = key
