82a7dae1f7d1cfe01115f5b7d1c69d7d6e89cd73
Daniel Tatsch <danielt.tatsch@gmail.com>
2021-08-15 21:36:31 -0300

    Add a hybrid sort algorithm with Insertion and Merge approaches

--- atividade1/sortingAlgorithms.py
+++ atividade1/sortingAlgorithms.py
@@ -40,3 +40,39 @@ def merge_sort(data):
             data[k] = right_list[j]
             j += 1
             k += 1
+
+def merge_insertion_sort(data, data_len, insertion_sort_level):
+    if len(data) > 1:
+        middle = data_len//2
+
+        if insertion_sort_level == 0:
+            insertion_sort(data)
+        else:
+            left_list  = data[:middle]
+            right_list = data[middle:]
+
+            merge_insertion_sort(left_list, len(left_list), insertion_sort_level - 1)
+            merge_insertion_sort(right_list, len(right_list), insertion_sort_level - 1)
+
+            i = 0
+            j = 0
+            k = 0
+
+            while i < len(left_list) and j < len(right_list):
+                if left_list[i] < right_list[j]:
+                    data[k] = left_list[i]
+                    i += 1
+                else:
+                    data[k] = right_list[j]
+                    j += 1
+                k += 1
+
+            while i < len(left_list):
+                data[k] = left_list[i]
+                i += 1
+                k += 1
+
+            while j < len(right_list):
+                data[k] = right_list[j]
+                j += 1
+                k += 1
