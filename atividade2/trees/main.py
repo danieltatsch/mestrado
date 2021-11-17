059d25f29c23c969a59113737ece1d5d7bbed569
Daniel Tatsch <danielt.tatsch@gmail.com>
2021-11-16 21:28:13 -0300

    Create application menu to test Insert and Delete methods

--- atividade2/trees/main.py
+++ atividade2/trees/main.py
@@ -1,5 +1,6 @@
 import random
 from trees import *
+import sys
 
 def avl_test():
     myTree = AVLTree()
@@ -24,7 +25,7 @@ def avl_test():
     except:
         pass
 
-def rb_test():
+def rb_avl_test():
     bst = RedBlackTree()
 
     bst.insert(55)
@@ -33,62 +34,121 @@ def rb_test():
     bst.insert(60)
     bst.insert(75)
     bst.insert(56)
-
-    # bst.print_tree()
-    bst.display()
-
-    print("\nAfter inserting one more element 57")
     bst.insert(57)
-    bst.display()
-
-    print("\nAfter inserting one more element 58")
     bst.insert(58)
-    bst.display()
-
-    print("\nAfter inserting one more element 59")
     bst.insert(59)
-    bst.display()
-
-    print("\nAfter inserting one more element 57")
     bst.insert(55)
-    bst.display()
-
-    print("\nAfter inserting one more element 57")
     bst.insert(41)
-    bst.display()
-
-    print("\nAfter inserting one more element 58")
     bst.insert(42)
-    bst.display()
-
-    print("\nAfter inserting one more element 59")
     bst.insert(43)
-    bst.display()
-
-    print("\nAfter inserting one more element 57")
     bst.insert(44)
-    bst.display()
-
-    print("\nAfter inserting one more element 57")
     bst.insert(45)
+    bst.delete_node(75)
+    bst.delete_node(60)
+    bst.delete_node(40)
+    bst.insert(70)
+    bst.insert(71)
+
+    print("ATUAL")
     bst.display()
 
+    print("\nAfter deleting an element 41")
+    bst.delete_node(41)
+    bst.display()
 
-    # print("\nAfter deleting an element")
-    # bst.delete_node(40)
-    # bst.display()
+    print("\nAfter deleting an element 57")
+    bst.delete_node(57)
+    bst.display()
 
-    # print("\nAfter deleting an element")
-    # bst.delete_node(55)
-    # bst.display()
+    print("\nAfter deleting an element 65")
+    bst.delete_node(65)
+    bst.display()
 
-    # print("\nAfter deleting an element")
-    # bst.delete_node(65)
-    # bst.display()
+    print("\nAfter deleting an element 59")
+    bst.delete_node(59)
+    bst.display()
+
+def insert_menu(rb_avl):
+    print("================================================================================")
+    while True:
+        key = input("Insert (or type BACK to return to main menu): ")
+
+        try:
+            key = int(key)
+        except:
+            if key.lower() == 'back':
+                print("================================================================================")
+                return
+
+            print("Invalid data, enter a number!")
+            continue
+
+        print("\nAfter insert element {}".format(key))
+
+        rb_avl.insert(key)
+        rb_avl.display()
+
+def delete_menu(rb_avl):
+    print("================================================================================")
+    while True:
+        key = input("Delete (or type BACK to return to main menu): ")
+
+        try:
+            key = int(key)
+        except:
+            if key.lower() == 'back':
+                print("================================================================================")
+                return
+
+            print("Invalid data, enter a number!")
+            continue
+
+        print("\nAfter delete element {}".format(key))
+
+        rb_avl.delete_node(key)
+        rb_avl.display()
+
+def menu():
+    option = -1
+    while option not in range(0,3):
+        print("=============================== AVL + RED BLACK ================================")
+        print("Choose an option:")
+        print("1. Insert")
+        print("2. Delete")
+        print("0. Exit")
+        print("================================================================================")
+      
+        try:
+            option = int(input())
+        except:
+            option = -1
+
+    return option
 
 def main():
     # avl_test()
-    rb_test()
+    # rb_avl_test()
+    rb_avl = RedBlackTree()
+
+    rb_avl.insert(60)
+    rb_avl.insert(50)
+    rb_avl.insert(40)
+    rb_avl.insert(30)
+    rb_avl.insert(20)
+    rb_avl.insert(10)
+    rb_avl.insert(5)
+
+    rb_avl.display()
+
+    while True:
+        option = menu()
+
+        if option == 1:
+            insert_menu(rb_avl)
+        elif option == 2:
+            delete_menu(rb_avl)
+        else:
+            sys.exit(1)
 
 if __name__ == "__main__":
     main()
