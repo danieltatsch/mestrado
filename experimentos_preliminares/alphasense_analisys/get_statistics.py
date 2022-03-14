596fedb7592e4532bbeb255fd4c129b54f03ae38
Daniel Tatsch <danielt.tatsch@gmail.com>
2022-03-14 00:08:45 -0300

    Add boxplot, histogram and correlation calc

--- experimentos_preliminares/alphasense_analisys/get_statistics.py
+++ experimentos_preliminares/alphasense_analisys/get_statistics.py
@@ -1,6 +1,9 @@
-import pandas as pd
-import numpy  as np
+import pandas  as pd
+import numpy   as np
+import seaborn as sns
+import matplotlib.pyplot as plt
 
+from scipy.stats  import pearsonr
 from pprint import pprint
 import json
 
@@ -41,16 +44,52 @@ climatic_element_obj = {
 output_dict = {}
 for gas in gas_obj:
     gas_df  = gas_obj[gas]
+    temp_df = climatic_element_obj['TEMP [ºC]']
+    rh_df   = climatic_element_obj['RH [%]']
 
-    greatter_than_range_count = 0
-    less_than_zero_count      = 0
+    gas_df_length  = gas_df.shape[0]
+    rh_df_length   = rh_df.shape[0]
+    temp_df_length = temp_df.shape[0]
 
+    # Set datasets to the same size
+    if gas_df_length < temp_df_length:
+        temp_df = temp_df[:gas_df_length]
+        rh_df   = rh_df[:gas_df_length]
+    elif gas_df_length > temp_df_length:
+        gas_df = gas_df[:temp_df_length]
+
+    print('GAS DF SIZE:  {}'.format(gas_df.shape[0]))
+    print("TEMP DF SIZE: {}".format(temp_df.shape[0]))
+    print("RH DF SIZE:   {}\n".format(rh_df.shape[0]))
+
+    print("-------------------------------")
+
+    # Count number of values outside the sensor range
     greatter_than_range_count = (gas_df['Value'] > gas_range_ppb[gas]).sum()
     less_than_zero_count      = (gas_df['Value'] < 0).sum()
 
+    # Get outside values range to exclude the correct indexes of RH and TEMP datasets
+    greatter_than_range_index = gas_df[gas_df['Value'] > gas_range_ppb[gas]].index.tolist()
+    less_than_range_index     = gas_df[gas_df['Value'] < 0].index.tolist()
+
+    # Remove indexes from RH and TEMP datasets
+    temp_df = temp_df.drop(temp_df.index[greatter_than_range_index])
+    rh_df   = rh_df.drop(rh_df.index[greatter_than_range_index])
+
+    temp_df = temp_df.drop(temp_df.index[less_than_range_index])
+    rh_df   = rh_df.drop(rh_df.index[less_than_range_index])
+
+    # Remove values outside the sensor range
     gas_df = gas_df[gas_df['Value'] <= gas_range_ppb[gas]]
     gas_df = gas_df[gas_df['Value'] >= 0]
 
+    # Correlation with outliers
+    corr_temp_w_outliers, _ = pearsonr(gas_df['Value'], temp_df['Value'])
+    corr_rh_w_outliers, _   = pearsonr(gas_df['Value'], rh_df['Value'])
+    # print('{} CORR TEMP: %.3f'.format(gas) % corr_temp_w_outliers)
+    # print('{} CORR RH:   %.3f\n'.format(gas) % corr_rh_w_outliers)
+
+    # Get variance
     gas_var = gas_df['Value'].var()
 
     # Get statistic metrics and transform to dict
@@ -64,6 +103,24 @@ for gas in gas_obj:
     outiler_lower_count = (gas_df['Value'] < lower_thr).sum()
     outiler_upper_count = (gas_df['Value'] > upper_thr).sum()
 
+    # Remove outliers from gas_df, temp_df and rh_df
+    gas_df_without_outliers = gas_df[gas_df['Value'] <= upper_thr]
+    gas_df_without_outliers = gas_df_without_outliers[gas_df_without_outliers['Value'] >= lower_thr]
+
+    outliers_indexes_list = gas_df[gas_df['Value'] >= upper_thr].index.tolist()
+    outliers_indexes_list = outliers_indexes_list + gas_df_without_outliers[gas_df_without_outliers['Value'] <= lower_thr].index.tolist()
+
+    # Correlation without outliers
+    temp_df = temp_df.drop(temp_df.index[outliers_indexes_list])
+    rh_df   = rh_df.drop(rh_df.index[outliers_indexes_list])
+
+    corr_temp_wo_outliers, _ = pearsonr(gas_df_without_outliers['Value'], temp_df['Value'])
+    corr_rh_wo_outliers, _   = pearsonr(gas_df_without_outliers['Value'], rh_df['Value'])
+    print('{} CORR TEMP: %.3f'.format(gas) % corr_temp_wo_outliers)
+    print('{} CORR RH:   %.3f\n'.format(gas) % corr_rh_wo_outliers)
+
+    input("FIM DA CORRELACAO SEM OS OUTLIERS")
+
     # Append additional information to dict
     statistics['var']                       = gas_var
     statistics['lower_thr']                 = lower_thr
@@ -72,8 +129,36 @@ for gas in gas_obj:
     statistics['outiler_upper_count']       = int(outiler_upper_count)
     statistics['greatter_than_range_count'] = int(greatter_than_range_count)
     statistics['less_than_zero_count']      = int(less_than_zero_count)
+    statistics['temp_cor_outliers']         = corr_temp_w_outliers
+    statistics['rh_cor_outlier']            = corr_rh_w_outliers
+
+    # boxplot without outliers
+
+    # sns.set_theme(style="whitegrid")
+    # ax = sns.boxplot(x=gas_df['Value'], showfliers = False)
+    # ax = sns.boxplot(x=gas_df['Value'])
+    # ax.axes.set_title('Boxplot - {}'.format(gas),fontsize=25)
+    # ax.set_xlabel('Concentração [ppb]',fontsize=25)
+    # ax.tick_params(axis='x', labelsize=15)
+    # plt.show()
+
+    # histogram
+
+    # sns.set_theme(style="whitegrid")
+    # ax = sns.histplot(x=gas_df_without_outliers['Value'])
+    # # ax = sns.histplot(x=gas_df['Value'])
+    # ax.axes.set_title('Histograma - {}'.format(gas),fontsize=25)
+    # ax.set_xlabel('Concentração [ppb]',fontsize=25)
+    # ax.set_ylabel('Quantidade de medições',fontsize=25)
+    # ax.tick_params(axis='x', labelsize=15)
+    # plt.show()
+
+    # correlation
+
+    # statistic analisys
 
     output_dict[gas] = statistics
+    # gas_obj[gas]     = gas_df
 
 pprint(output_dict)
 
